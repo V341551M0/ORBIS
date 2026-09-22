3188bab190db826578dc68dc343d260deaf786aa
Kauã Veríssimo <187907796+V341551M0@users.noreply.github.com>
2026-09-22 13:22:25 -0500

    feat(backend): establish backend architecture

---
+++ apps/api/src/main/java/com/orbis/ApplicationOrbis.java
@@ -0,0 +1,12 @@
+package com.orbis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationOrbis {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrbisApplication.class, args);
+    }
+}
